84fb530f523ba62b1f73ee27d77f3ea8e19c9853
Manikanta L.S.E <manikanta.lse@gmail.com>
2018-12-13 18:37:39 +0530

    Removing results after text is deleted

--- src/components/routes/Search_Page.js
+++ src/components/routes/Search_Page.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 class Search_Page extends React.Component {
 
 
-// initializing the state
+  // initializing the state
   constructor(props) {
     super(props);
     this.state = {
@@ -16,37 +16,45 @@ class Search_Page extends React.Component {
     };
   }
 
-// request for getting all the book data from the server
+  // request for getting all the book data from the server
   componentDidMount() {
     BooksAPI.getAll().then(books => {
       this.setState({ books: books });
     });
   }
 
-// updating the state 
+  // updating the state 
   updateQuery = query => {
     this.setState({ query: query }, () => this.submitSearch(query));
   };
 
-// submitting the search keyword to the server
+  // submitting the search keyword to the server
   submitSearch(query) {
+
+    // trimming the whitespaces
+    query = query.trim();
+
+    // if the text is deleted search results are not shown
     if (query === "" || query === undefined) {
       return this.setState({ results: [] });
-    }
+     }
 
     // searching for the associated books
-    BooksAPI.search(this.state.query.trim()).then(result => {
+    BooksAPI.search(this.state.query).then(result => {
 
-      if (result.error) {
-
+    
+      const searchQuery = document.querySelector("#search").value;
+      
+      // checking the value of search box if a request arrives late
+      if (result.error || searchQuery === "" || searchQuery === undefined) {
         return this.setState({ results: [] });
       } else {
 
         result.forEach(book => {
           let filteredItems = this.state.books.filter(Book => Book.id === book.id);
-          book.shelf = filteredItems.length>0 ? filteredItems.shelf : "none";
+          book.shelf = filteredItems.length > 0 ? filteredItems.shelf : "none";
 
-          if (filteredItems.length>0) {
+          if (filteredItems.length > 0) {
             book.shelf = filteredItems[0].shelf;
           }
         });
@@ -75,6 +83,7 @@ class Search_Page extends React.Component {
           </Link>
           <div className="search-books-input-wrapper">
             <input
+              id="search"
               type="text"
               placeholder="Search by title or author"
               value={this.state.query}
@@ -84,8 +93,8 @@ class Search_Page extends React.Component {
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            {/* mapping each returned book with unique id */}
-            {this.state.results.map((book, id) => <Book setBook={this.setBook} key={id} book={book}  />)}
+            {/* mapping each returned book with unique id and assigning into state results */}
+            {this.state.results.map((book, id) => <Book setBook={this.setBook} key={id} book={book} />)}
           </ol>
         </div>
       </div>
